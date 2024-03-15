b52855227a271ab6b1393f85345e42af4dad0ea2
Alex Jenter <jenteralex@gmail.com>
2024-03-15 19:40:25 +0100

    Add some custom config for markdown
    still flaky but figured out where to put the file at least

---
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,14 @@
+-- print("hello markdown")
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.number = false
+vim.opt.textwidth = 32
+vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true, silent = false })
+vim.g.markdown_fenced_languages = {
+	"bash=sh",
+	"console=sh",
+	"lua",
+	"zsh=sh",
+}
