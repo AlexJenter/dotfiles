6f7a2543f0849c985d13b487459fea6d93d93deb
Alex Jenter <jenteralex@gmail.com>
2024-03-16 17:16:44 +0100

    remove hard line breaks in markdown files

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -3,7 +3,6 @@
 vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.number = false
-vim.opt.textwidth = 32
 vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true, silent = false })
 vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true, silent = false })
 vim.g.markdown_fenced_languages = {
