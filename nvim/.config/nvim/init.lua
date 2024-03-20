c331a67a81c9611290b420ac7615999e3a3662d6
Alex Jenter <jenteralex@gmail.com>
2024-03-20 10:11:49 +0100

    Move which-key config and add first neogit keymap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -188,45 +188,6 @@ require("lazy").setup({
 		},
 	},
 
-	-- NOTE: Plugins can also be configured to run lua code when they are loaded.
-	--
-	-- This is often very useful to both group configuration, as well as handle
-	-- lazy loading plugins that don't need to be loaded immediately at startup.
-	--
-	-- For example, in the following configuration, we use:
-	--  event = 'VimEnter'
-	--
-	-- which loads which-key before all the UI elements are loaded. Events can be
-	-- normal autocommands events (`:help autocmd-events`).
-	--
-	-- Then, because we use the `config` key, the configuration only runs
-	-- after the plugin has been loaded:
-	--  config = function() ... end
-
-	{ -- Useful plugin to show you pending keybinds.
-		"folke/which-key.nvim",
-		event = "VimEnter", -- Sets the loading event to 'VimEnter'
-		config = function() -- This is the function that runs, AFTER loading
-			require("which-key").setup()
-
-			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			})
-		end,
-	},
-
-	-- NOTE: Plugins can specify dependencies.
-	--
-	-- The dependencies are proper plugin specifications as well - anything
-	-- you do for a plugin at the top level, you can do for a dependency.
-	--
-	-- Use the `dependencies` key to specify the dependencies of a particular plugin
-
 	{ -- Fuzzy Finder (files, lsp, etc)
 		"nvim-telescope/telescope.nvim",
 		event = "VimEnter",
@@ -742,6 +703,7 @@ require("lazy").setup({
 	{ import = "alex.plugins.colorscheme" },
 	{ import = "alex.plugins.nvim-tree" },
 	{ import = "alex.plugins.neogit" },
+	{ import = "alex.plugins.which-key" },
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
---
+++ nvim/.config/nvim/lua/alex/plugins/which-key.lua
@@ -0,0 +1,25 @@
+return { -- Useful plugin to show you pending keybinds.
+	"folke/which-key.nvim",
+	event = "VimEnter", -- Sets the loading event to 'VimEnter'
+	config = function() -- This is the function that runs, AFTER loading
+		require("which-key").setup()
+
+		-- Document existing key chains
+		local wk = require("which-key")
+
+		wk.register({
+			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+		})
+		wk.register({
+			["<leader>G"] = {
+				name = "[G]it",
+				G = { "<cmd>Neogit<cr>", "Open Neo[g]it" },
+				_ = "which_key_ignore",
+			},
+		})
+	end,
+}
