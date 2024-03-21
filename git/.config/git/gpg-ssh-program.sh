57757c021824bc41d4028f1ec1e8ae268d7c0b65
Alex Jenter <jenteralex@gmail.com>
2024-03-21 20:42:01 +0100

    IDEA kj from the 1password slack thought of this
    https://1password-devs.slack.com/archives/C032QPCFK0B/p1711032105504379

---
+++ git/.config/git/gpg-ssh-program.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+MACOS_SSH_SIGN="/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+LINUX_SSH_SIGN="/opt/1Password/op-ssh-sign"
+
+if [ -f "$MACOS_SSH_SIGN" ]; then
+  "$MACOS_SSH_SIGN" "$@"
+elif [ -f "$LINUX_SSH_SIGN" ]; then
+  "$LINUX_SSH_SIGN" "$@"
+else
+    ssh-keygen "$@"
+fii
